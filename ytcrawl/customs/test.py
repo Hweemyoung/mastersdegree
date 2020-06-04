174668539dfd3fea8dfd2bbfe7fcad30a433893b
hweem <hweemyoung@gmail.com>
2020-06-04 21:33:45 +0900

    updated

--- ytcrawl/customs/test.py
+++ ytcrawl/customs/test.py
@@ -20,6 +20,7 @@ from altmetric_it import AltmetricIt
 
 import json
 import os
+import statistics
 
 
 def _check_arxiv_id_exists():
@@ -50,6 +51,7 @@ def _temp():
 def test_plt(num_cols, max_fig=30):
     db_handler = DBHandler()
     _list_idx_arxiv = [_fname[:-4] for _fname in os.listdir('./stats/videos')]
+    print('# of video stats files:', len(_list_idx_arxiv))
     shuffle(_list_idx_arxiv)
     _num_fig = 0
     _iter_idx_arxiv = iter(_list_idx_arxiv)
@@ -66,7 +68,8 @@ def test_plt(num_cols, max_fig=30):
         with open('./stats/videos/%s.txt' % _idx_arxiv, 'r') as f:
             _dict_stats_videos = json.load(f)
         db_handler.sql_handler.select(
-            'papers_cs', 'altmetric_id').where('idx_arxiv', _idx_arxiv)
+            # 'papers_cs', 'altmetric_id').where('idx_arxiv', _idx_arxiv)
+            'papers_cs', 'altmetric_id').where('idx_arxiv', _idx_arxiv).where('subject_1', 'Computer Science', '=').where('subject_2', 'Machine Learning', '=')
 
         altmetric_id = db_handler.execute().fetchall()[0][0]
         if altmetric_id == None:
@@ -92,15 +95,15 @@ def test_plt(num_cols, max_fig=30):
                                  ) if _key in _dict_stats_videos['videos']['videoCount'] else 0
             y_vals_videoCount.append(val_videoCount)
             # val_explanationCount = int(_dict_stats_videos['videos']['content'][_key]['paper_explanation']
-                                #  ) if _key in _dict_stats_videos['videos']['videoCount'] else 0
+            #  ) if _key in _dict_stats_videos['videos']['videoCount'] else 0
             # y_vals_explanationCount.append(val_explanationCount)
             val_tweetCount = int(_dict_stats_twitter['twitter']['count_tweets'][_key]
                                  ) if _key in _dict_stats_twitter['twitter']['count_tweets'] else 0
             y_vals_tweetCount.append(val_tweetCount)
 
-        x_vals = [val[2:] for val in x_vals] # YYYY-MM to YY-MM
+        x_vals = [val[2:] for val in x_vals]  # YYYY-MM to YY-MM
         primary_axis = axes[_num_fig // num_cols, _num_fig % num_cols]
-        primary_axis.set_title(_idx_arxiv, fontsize=10) # Title: idx_arxiv
+        primary_axis.set_title(_idx_arxiv, fontsize=10)  # Title: idx_arxiv
         primary_axis.set_ylim([0.1, 100])  # Tweets
         primary_axis.plot(x_vals, y_vals_tweetCount, 'b-')
         primary_axis.set_yscale('log')
@@ -119,9 +122,162 @@ def test_plt(num_cols, max_fig=30):
     plt.show()
 
 
+def interval_from_paper():
+    regex_idx_arxiv = re.compile(r'\d{3,5}.\d{3,5}')
+    _list_content = ['paper_explanation', 'paper_reference', 'paper_supplementary',
+                     'news', 'paper_application', 'dialogue', 'routine', 'paper_assessment']
+    _dict_xlim = {'timedelta': [-30, 500],
+                  'duration': [0, 150], 'viewCount': [1, 2000]}
+
+    # dict_attr_by_content
+    _dict_td_by_content = dict()
+    _dict_viewCount_by_content = dict()
+    _dict_duration_by_content = dict()
+
+    _list_cols = ['duration', 'viewCount', 'content', 'q', 'publishedAt']
+    db_handler = DBHandler()
+    db_handler.sql_handler.select('videos_cs.LG', _list_cols)
+    _videos_LG = db_handler.execute().fetchall()
+    for _row in _videos_LG:
+        _dict_video = dict(zip(_list_cols, _row))
+        _idx_arxiv = regex_idx_arxiv.findall(_dict_video['q'])[0]
+        db_handler.sql_handler.select(
+            'papers_cs', 'publishedAt').where('idx_arxiv', _idx_arxiv)
+        _d_paper = db_handler.execute().fetchall()[0][0]
+        if _d_paper == None:
+            continue
+
+        if _dict_video['content'] not in _dict_td_by_content:
+            _dict_td_by_content[_dict_video['content']] = list()
+            _dict_viewCount_by_content[_dict_video['content']] = list()
+            _dict_duration_by_content[_dict_video['content']] = list()
+
+        # viewCount
+        # Filter by viewCount?
+        # if int(_dict_video['viewCount']) < 500:
+            # continue
+        _dict_viewCount_by_content[_dict_video['content']].append(
+            int(_dict_video['viewCount']))
+
+        # timedelta
+        _days_td = (_dict_video['publishedAt'].date() - _d_paper).days
+        # Filter by timedelta?
+        # if _days_td < 180:
+        # continue
+        _dict_td_by_content[_dict_video['content']].append(_days_td)
+
+        # duration
+        _dict_duration_by_content[_dict_video['content']].append(
+            int(_dict_video['duration']) / 60)
+
+    _dict_stats_by_content = dict()
+    for _content in _dict_td_by_content:
+        _dict_stats_by_content[_content] = dict()
+        # timedelta
+        _dict_stats_by_content = _add_stats(
+            _dict_stats_by_content, _dict_td_by_content, 'timedelta', filter_outliers=True)
+        # viewCount
+        _dict_stats_by_content = _add_stats(
+            _dict_stats_by_content, _dict_viewCount_by_content, 'viewCount', filter_outliers=True)
+        # duration
+        _dict_stats_by_content = _add_stats(
+            _dict_stats_by_content, _dict_duration_by_content, 'duration', filter_outliers=True)
+        # N
+        _dict_stats_by_content[_content]['n'] = len(
+            _dict_td_by_content[_content])
+
+    # print('Dict stats:\n', _dict_stats_by_content)
+    for _content in _dict_stats_by_content:
+        print('duration for', _content,
+              _dict_stats_by_content[_content]['duration'])
+        print('viewCount for', _content,
+              _dict_stats_by_content[_content]['viewCount'])
+        print('timedelta for', _content,
+              _dict_stats_by_content[_content]['timedelta'])
+
+    _dict_by_attr = dict()
+    _dict_by_attr['timedelta'] = _dict_td_by_content
+    _dict_by_attr['viewCount'] = _dict_viewCount_by_content
+    _dict_by_attr['duration'] = _dict_duration_by_content
+
+    # Histogram
+    fig, axes = plt.subplots(3, 8)
+    for _i, _attr in enumerate(_dict_by_attr):
+        # for _j, _content in enumerate(_list_content):
+        for _j, _content in enumerate(_list_content):
+            primary_axis = axes[_i, _j]
+            if _i == 0:
+                primary_axis.set_title('%s(N=%d)' % (_content, len(
+                    _dict_by_attr[_attr][_content]) if _content in _dict_by_attr[_attr] else 0), fontsize=10)  # paper_explanation
+            if _j == 0:
+                primary_axis.set_ylabel(_attr, fontsize=10)  # duration
+            # Remove outliers
+            # reject_outliers(np.array(_dict_by_attr[_attr][_content]), m=3.0)
+            if _content in _dict_by_attr[_attr]:
+                print('attr:', _attr, 'content:', _content)
+                print(_dict_by_attr[_attr][_content])
+                primary_axis.hist(_dict_by_attr[_attr][_content], bins=10)
+            else:
+                primary_axis.hist([])
+            primary_axis.set_ylim([0, 20])
+            # primary_axis.set_xlim(_dict_xlim[_attr])
+            # if _attr == 'viewCount':
+            # primary_axis.set_xscale('log')
+
+            # primary_axis.plot(x_vals, y_vals_tweetCount, 'b-')
+            # primary_axis.set_yscale('log')
+            # primary_axis.set_xticklabels(labels=x_vals, rotation=90)
+
+            # primary_axis.set_xlabel('Month')
+            # primary_axis.set_ylabel('video')
+
+            # secondary_axis = primary_axis.twinx()
+            # secondary_axis.plot(x_vals, y_vals_videoCount, 'g--')
+            # secondary_axis.set_ylim([0.1, 10000])  # Videos
+            # secondary_axis.set_yscale('log')
+    plt.show()
+
+
+def reject_outliers(data, m=3.):
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0.
+    return data[s < m]
+
+
+def _add_stats(dict_stats_by_content, dict_attr_by_content, attribute='timedelta', filter_outliers=True):
+    for _content in dict_stats_by_content:
+        dict_stats_by_content[_content][attribute] = dict()
+
+        if filter_outliers:
+            dict_attr_by_content[_content] = reject_outliers(
+                np.sort(np.array(dict_attr_by_content[_content])), m=8.0)
+            dict_attr_by_content[_content] = dict_attr_by_content[_content].flatten(
+            )
+
+        dict_stats_by_content[_content][attribute]['n'] = len(
+            dict_attr_by_content[_content])
+        try:
+            _mean = statistics.mean(dict_attr_by_content[_content])
+        except:
+            dict_stats_by_content[_content][attribute]['mean'] = None
+        else:
+            dict_stats_by_content[_content][attribute]['mean'] = round(
+                _mean, 1)
+        try:
+            _std = statistics.stdev(dict_attr_by_content[_content])
+        except:
+            dict_stats_by_content[_content][attribute]['std'] = None
+        else:
+            dict_stats_by_content[_content][attribute]['std'] = round(
+                _std, 2)
+    return dict_stats_by_content
+
+
 if __name__ == '__main__':
     # _temp()
-    test_plt(num_cols=6, max_fig=20)
+    # test_plt(num_cols=6, max_fig=20)
+    interval_from_paper()
     # sql_handler = SQLHandler()
 
     # with open('table.csv', newline='') as f:
